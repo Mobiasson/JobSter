016e398cc26af9f9e180c381bf32fff4a1040374
Mobiasson <mikaeltobiasson@hotmail.com>
2026-01-23 12:54:43 +0100

    Added custom close button for login

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -1,16 +1,24 @@
 ﻿using System.Windows;
 using JobSter.ViewModels;
 
-namespace JobSter.Views; 
-public partial class LoginView : Window {
-    public LoginView() {
+namespace JobSter.Views;
+public partial class LoginView : Window
+{
+    public LoginView()
+    {
         InitializeComponent();
         DataContext = new LoginViewModel();
     }
 
-    private void btnLogin_Click(object sender, RoutedEventArgs e) {
+    private void btnLogin_Click(object sender, RoutedEventArgs e)
+    {
         var passwordView = new PasswordView();
         passwordView.Show();
 
+    }
+
+    private void btn_Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
