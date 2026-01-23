4ddfba2a3c6b64e5c762bb0947fc48a195d6f067
Mobiasson <mikaeltobiasson@hotmail.com>
2026-01-23 15:38:51 +0100

    Fixed buttons for login and password window

--- Views/PasswordView.xaml.cs
+++ Views/PasswordView.xaml.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace JobSter.Views
-{
-    /// <summary>
-    /// Interaction logic for PasswordView.xaml
-    /// </summary>
-    public partial class PasswordView : Window
-    {
-        public PasswordView()
-        {
-            InitializeComponent();
-        }
+namespace JobSter.Views;
+public partial class PasswordView : Window {
+    public PasswordView() {
+        InitializeComponent();
     }
+
+    private void btn_Close_Click(object sender, RoutedEventArgs e) {
+        Close();
+    }
+
 }
