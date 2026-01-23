1ee4c8fc71da7d92e05c44f8e9f951e13ad09ec6
Mobiasson <mikaeltobiasson@hotmail.com>
2026-01-23 11:15:07 +0100

    Fixed somewhat working LoginScreen

---
+++ Model/User.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace JobSter.Model;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("username")]
+    public string Username { get; set; } = string.Empty;
+
+    [BsonElement("pinHash")]
+    public string PinHash { get; set; } = string.Empty;
+
+    [BsonElement("pinSalt")]
+    public string PinSalt { get; set; } = string.Empty;
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
