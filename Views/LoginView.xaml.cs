052986f7ed46919edf24dc5cb6a5f5f5b7874002
Mobiasson <mikaeltobiasson@hotmail.com>
2026-01-23 13:25:11 +0100

    Added custom minimize, maximize and close buttons. Looks good!

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -2,23 +2,38 @@
 using JobSter.ViewModels;
 
 namespace JobSter.Views;
-public partial class LoginView : Window
-{
-    public LoginView()
-    {
+public partial class LoginView : Window {
+    public LoginView() {
         InitializeComponent();
         DataContext = new LoginViewModel();
     }
 
-    private void btnLogin_Click(object sender, RoutedEventArgs e)
-    {
+    private void btnLogin_Click(object sender, RoutedEventArgs e) {
         var passwordView = new PasswordView();
         passwordView.Show();
 
     }
 
-    private void btn_Close_Click(object sender, RoutedEventArgs e)
-    {
+    private void btn_Close_Click(object sender, RoutedEventArgs e) {
         Close();
+    }
+
+    private void btn_Maximize_Click(object sender, RoutedEventArgs e) {
+        if(this.WindowState == WindowState.Normal) {
+            this.WindowState = WindowState.Maximized;
+        } else {
+            this.WindowState = WindowState.Normal;
+        }
+
+    }
+
+    private void btn_Minimize_Click(object sender, RoutedEventArgs e) {
+        if(this.WindowState == WindowState.Normal) {
+            this.WindowState = WindowState.Minimized;
+        } else if(this.WindowState == WindowState.Maximized) {
+            this.WindowState = WindowState.Minimized;
+        } else {
+            this.WindowState = WindowState.Normal;
+        }
     }
 }
